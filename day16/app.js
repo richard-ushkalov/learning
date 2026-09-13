8ff6560e9f254816998ca9cc84e24eaf6ab25a6f
Richard Ushkalov <318159922+richard-ushkalov@users.noreply.github.com>
2026-09-13 19:19:19 +0300

    refactor: change players data

--- day16/app.js
+++ day16/app.js
@@ -1,9 +1,9 @@
 const players = [
-  { id: 'p1', name: 'Аня', team: 'красные', potted: 8, fouls: 3, isActive: false },
-  { id: 'p2', name: 'Борис', team: 'красные', potted: 8, fouls: 1, isActive: false },
-  { id: 'p3', name: 'Вера', team: 'синие', potted: 3, fouls: 2, isActive: false },
+  { id: 'p1', name: 'Аня', team: 'красные', potted: 8, fouls: 3, isActive: true },
+  { id: 'p2', name: 'Борис', team: 'красные', potted: 8, fouls: 1, isActive: true },
+  { id: 'p3', name: 'Вера', team: 'синие', potted: 3, fouls: 2, isActive: true },
   { id: 'p4', name: 'Глеб', team: 'синие', potted: 5, fouls: 0, isActive: false },
-  { id: 'p5', name: 'Дина', team: 'зелёные', potted: 0, fouls: 4, isActive: false },
+  { id: 'p5', name: 'Дина', team: 'зелёные', potted: 0, fouls: 4, isActive: true },
 ];
 
 const block = {
