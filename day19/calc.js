684009aa768a33b03085bf3c36188336947a23c7
Richard Ushkalov <318159922+richard-ushkalov@users.noreply.github.com>
2026-09-16 19:49:23 +0300

    refactor: change export style

--- day19/calc.js
+++ day19/calc.js
@@ -1,11 +1,11 @@
 const calcPlayerScore = (potted, fouls) => { return potted * 10 - fouls * 5; };
-const calcActivePlayers = players => { return players.filter(player => player.isActive).length; };
-const calcActivePotted = players => {
+export const calcActivePlayers = players => { return players.filter(player => player.isActive).length; };
+export const calcActivePotted = players => {
   return players
     .filter(player => player.isActive)
     .reduce((potted, player) => potted + player.potted, 0);
 };
-const calcActiveAverageFouls = players => {
+export const calcActiveAverageFouls = players => {
   const activePlayers = players.filter(player => player.isActive);
   const activeCount = activePlayers.length;
 
@@ -15,17 +15,17 @@ const calcActiveAverageFouls = players => {
   return (activeFouls / activeCount).toFixed(1);
 };
 
-const getActiveSortedPlayers = players => {
+export const getActiveSortedPlayers = players => {
   return players
     .filter(player => player.isActive)
     .sort((a, b) => b.potted - a.potted === 0 ? a.fouls - b.fouls : b.potted - a.potted);
 };
 
-const getPlayersWithScore = players => {
+export const getPlayersWithScore = players => {
   return players.map(player => ({ ...player, score: calcPlayerScore(player.potted, player.fouls) }));
 };
 
-const getPlayersByTeams = players => {
+export const getPlayersByTeams = players => {
   return players
     .reduce((teams, player) => {
       teams[player.team] = teams[player.team] ?? [];
@@ -34,5 +34,3 @@ const getPlayersByTeams = players => {
       return teams;
     }, {});
 };
-
-export { calcActivePlayers, calcActivePotted, calcActiveAverageFouls, getActiveSortedPlayers, getPlayersWithScore, getPlayersByTeams }
